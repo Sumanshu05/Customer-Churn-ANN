3b04f229b1cd863ea3a515259030e246c078c07b
Sumanshu Nagpal <nagpalsumanshu@gmail.com>
2026-07-22 20:39:10 +0530

    Fixed Deployment

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ model = tf.keras.models.load_model('model.h5')
 with open('label_encoder_gender.pkl' , 'rb') as file:
     label_encoder_gender = pickle.load(file)
     
-with open('onehot_encoder_ge0.pkl' , 'rb') as file:
+with open('onehot_encoder_geo.pkl' , 'rb') as file:
     onehot_encoder_geo = pickle.load(file)
     
 with open('scaler.pkl' , 'rb') as file:
@@ -39,7 +39,7 @@ is_active_member = st.selectbox('Is Active Member' , [0,1])
 # Prepare the Input Data 
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
-    'Gender': [label_encoder_gender.transfrom([gender])[0]],
+    'Gender': [label_encoder_gender.transform([gender])[0]],
     'Age': [age],
     'Tenure': [tenure],
     'Balance': [balance],
